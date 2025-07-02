c2ed4da737631a13e6c9042bff65cd9a398e4417
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-07-02 16:40:36 -0700

    initial build 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
   description: "Official portfolio of a prominent fashion designer.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
